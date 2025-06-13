c0ef260032b3a7afa9dbf5f6b3ceae4bf68b8c16
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-06-13 09:15:03 +0530

    This is my java scanner of addition program

---
+++ C2TC_CoreJavaPrograms/src/day1/ScannerJava.java
@@ -0,0 +1,20 @@
+package day1;
+import java.util.Scanner;
+public class ScannerJava{
+
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		Scanner ac;
+		ac= new Scanner(System.in);
+		System.out.println("enter a ");
+		a = ac.nextInt();
+		System.out.println("enter b");
+		b = ac.nextInt();
+		c = a+b;
+		System.out.println("additon of a+b = "+c);
+		
+		
+	}
+
+}

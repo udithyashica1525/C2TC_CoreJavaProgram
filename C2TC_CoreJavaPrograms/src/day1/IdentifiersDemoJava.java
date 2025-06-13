fad15a3b43fc62bf08ec9b0d0ae82810a4a8136b
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-06-13 10:34:59 +0530

    This is my  identifier demo java program

---
+++ C2TC_CoreJavaPrograms/src/day1/IdentifiersDemoJava.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class IdentifiersDemoJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           int $number = 20;
+           System.out.println("value of the number variable is : "+ $number);
+           
+           String studentName = "Aniket";
+           System.out.println("value is :"+ studentName);
+	}
+
+}

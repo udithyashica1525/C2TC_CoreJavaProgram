73d5bbe2f62ad2abb25a977b1dd2b8866d00b90f
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-06-16 13:53:45 +0530

    This is my break dmeo java program

---
+++ C2TC_CoreJavaPrograms/src/day2/BreakDemoJava.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class BreakDemoJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 5; i < 10; i++) {
+			if (i == 5)
+				break;
+			System.out.println(i);
+		}
+	}
+}

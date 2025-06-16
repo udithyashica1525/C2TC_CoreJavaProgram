7eee87cda659d9f89894ffb13a16a54c84e2c608
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-06-16 14:01:10 +0530

    This is my do while demo java program

---
+++ C2TC_CoreJavaPrograms/src/day2/DoWhileDemoJava.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class DoWhileDemoJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 11;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 1);
+	}
+
+}

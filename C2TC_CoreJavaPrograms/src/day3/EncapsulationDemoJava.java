d27b32a288d2fdbf6aefe4711e0d4c685b2beeab
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-06-16 15:19:47 +0530

    This is my Encapsulation in oops concept demo java program

---
+++ C2TC_CoreJavaPrograms/src/day3/EncapsulationDemoJava.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class EncapsulationDemoJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+	}
+
+}

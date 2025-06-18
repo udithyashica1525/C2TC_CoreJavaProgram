0336f789eca04314b6dc78313351550853ed86d8
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-06-18 11:29:11 +0530

    This is my singleton java program using private acces specifier

---
+++ C2TC_CoreJavaPrograms/src/day4/Student.java
@@ -0,0 +1,73 @@
+package day4;
+
+public class Student {
+	private static Student s1= new Student();
+	
+    public static Student getS1() {
+		return s1;
+	}
+
+	private int id;
+    private String name;
+ 
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	}
+	private Student() {
+    	super();
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    }
+    
+}
---
+++ C2TC_CoreJavaPrograms/src/day4/StudentDemo.java
@@ -0,0 +1,28 @@
+package day4;
+
+public class StudentDemo {
+
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Student s= Student.getS1();
+         
+         s.setId(101);
+         s.setName("Jai");
+         
+         Student s1 = Student.getS1();
+         
+         System.out.println(s1);
+         System.out.println(s);
+	}
+
+	
+		// TODO Auto-generated constructor stub
+	
+
+}

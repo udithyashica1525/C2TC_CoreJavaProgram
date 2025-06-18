ce9e6234ff6fe567fdb6ebef4a713ac6093f7692
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-06-18 11:31:20 +0530

    This is my private constructor java program

---
+++ C2TC_CoreJavaPrograms/src/day4/MyClass.java
@@ -0,0 +1,29 @@
+package day4;
+
+public class MyClass {
+      private static MyClass obj=new MyClass();
+      private int id;
+      
+      public int getId()
+      {
+    	  return id;
+      }
+      
+      public void setId(int id) {
+    	  this.id=id;
+      }
+       
+      private MyClass() {
+    	  System.out.println("MyClass object created");
+      }
+      
+      public static MyClass getObject(){
+  		return obj;
+  	}
+
+  	@Override
+  	public String toString() {
+  		return "MyClass [id=" + id + "]";
+  	}
+  	
+}
---
+++ C2TC_CoreJavaPrograms/src/day4/PrivateConstructorDemo.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class PrivateConstructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ MyClass m = MyClass.getObject();
+ m.setId(10);
+ 
+ MyClass m1 = MyClass.getObject();
+ 
+ System.out.println(m);
+ System.out.println(m1);
+ 
+	}
+
+}

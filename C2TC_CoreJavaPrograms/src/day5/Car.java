5d39da467984bcaba2cd02dc8a2b80e366696e2f
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-06-23 13:58:50 +0530

    This is my multilevel inheritance java program

---
+++ C2TC_CoreJavaPrograms/src/day5/Car.java
@@ -0,0 +1,11 @@
+package day5;
+
+public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
+}
---
+++ C2TC_CoreJavaPrograms/src/day5/Maruti800.java
@@ -0,0 +1,11 @@
+package day5;
+
+public class Maruti800 extends Maruti {
+	public Maruti800() {
+		System.out.println("Maruti Model: 800");
+	}
+
+	public void speed() {
+		System.out.println("Max: 80Kmph");
+	}
+}

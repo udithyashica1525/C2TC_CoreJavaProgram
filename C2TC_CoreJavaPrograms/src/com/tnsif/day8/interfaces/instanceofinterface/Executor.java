d14af78070679d9b5af693c199c640c72dc605ee
ACT-C1-23 <ACT-C1-23@DESKTOP-I4SCAC5>
2025-06-24 11:31:42 +0530

    This is my instance of interface java program

---
+++ C2TC_CoreJavaPrograms/src/com/tnsif/day8/interfaces/instanceofinterface/Executor.java
@@ -0,0 +1,16 @@
+package com.tnsif.day8.interfaces.instanceofinterface;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Phone p1 = PhoneFactory.createPhone("Samsung"); 
+		p1.call();
+		p1.sms();
+		
+		p1 = PhoneFactory.createPhone("Jio"); 
+		p1.call();
+		p1.sms();
+	}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/com/tnsif/day8/interfaces/instanceofinterface/Phone.java
@@ -0,0 +1,7 @@
+package com.tnsif.day8.interfaces.instanceofinterface;
+
+public interface Phone {
+       void call();
+       
+       void sms();
+}
